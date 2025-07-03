c0664edd24f6bd8a6ae53bf8a621248f21ffb087
ehdnd_mac <taeungjang@naver.com>
2025-07-03 23:08:23 +0900

    feat: testing scribble package

--- lib/main.dart
+++ lib/main.dart
@@ -1,144 +1,345 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:scribble/scribble.dart';
+import 'package:value_notifier_tools/value_notifier_tools.dart';
 
-/// The main entry point of the application.
-///
-/// This function initializes the Flutter application and runs [MyApp].
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
-/// The root widget of the application.
-///
-/// This is a [StatelessWidget] that serves as the top-level widget
-/// for the entire app, setting up the [MaterialApp] configuration.
 class MyApp extends StatelessWidget {
-  /// Creates a [MyApp] widget.
-  ///
-  /// The [key] parameter is passed to the superclass constructor.
   const MyApp({super.key});
 
-  /// Builds the widget tree for this [MyApp].
-  ///
-  /// Returns a [MaterialApp] with the app's theme, title, and home page.
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'IT Contest Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      title: 'Scribble',
+      theme: ThemeData.from(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
       ),
-      home: const MyHomePage(title: 'IT Contest Flutter Demo Home Page'),
+      home: const HomePage(title: 'Scribble'),
     );
   }
 }
 
-/// The home page widget that displays the main content.
-///
-/// This is a [StatefulWidget] that shows a counter example
-/// with an app bar and floating action button.
-class MyHomePage extends StatefulWidget {
-  /// Creates a [MyHomePage] widget.
-  ///
-  /// The [title] parameter sets the text displayed in the app bar.
-  /// The [key] parameter is passed to the superclass constructor.
-  const MyHomePage({super.key, required this.title});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title});
 
-  /// The title displayed in the app bar.
   final String title;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage> {
+  late ScribbleNotifier notifier;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    notifier = ScribbleNotifier();
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        actions: _buildActions(context),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 64),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          children: [
+            Expanded(
+              child: Card(
+                clipBehavior: Clip.hardEdge,
+                margin: EdgeInsets.zero,
+                color: Colors.white,
+                surfaceTintColor: Colors.white,
+                child: Scribble(
+                  notifier: notifier,
+                  drawPen: true,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Row(
+                children: [
+                  _buildColorToolbar(context),
+                  const VerticalDivider(width: 32),
+                  _buildStrokeToolbar(context),
+                  const Expanded(child: SizedBox()),
+                  _buildPointerModeSwitcher(context),
+                ],
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  List<Widget> _buildActions(context) {
+    return [
+      ValueListenableBuilder(
+        valueListenable: notifier,
+        builder: (context, value, child) => IconButton(
+          icon: child as Icon,
+          tooltip: 'Undo',
+          onPressed: notifier.canUndo ? notifier.undo : null,
+        ),
+        child: const Icon(Icons.undo),
+      ),
+      ValueListenableBuilder(
+        valueListenable: notifier,
+        builder: (context, value, child) => IconButton(
+          icon: child as Icon,
+          tooltip: 'Redo',
+          onPressed: notifier.canRedo ? notifier.redo : null,
+        ),
+        child: const Icon(Icons.redo),
+      ),
+      IconButton(
+        icon: const Icon(Icons.clear),
+        tooltip: 'Clear',
+        onPressed: notifier.clear,
+      ),
+      IconButton(
+        icon: const Icon(Icons.image),
+        tooltip: 'Show PNG Image',
+        onPressed: () => _showImage(context),
+      ),
+      IconButton(
+        icon: const Icon(Icons.data_object),
+        tooltip: 'Show JSON',
+        onPressed: () => _showJson(context),
+      ),
+    ];
+  }
+
+  void _showImage(BuildContext context) async {
+    final image = notifier.renderImage();
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Generated Image'),
+        content: SizedBox.expand(
+          child: FutureBuilder(
+            future: image,
+            builder: (context, snapshot) => snapshot.hasData
+                ? Image.memory(snapshot.data!.buffer.asUint8List())
+                : const Center(child: CircularProgressIndicator()),
+          ),
+        ),
+        actions: [
+          TextButton(
+            onPressed: Navigator.of(context).pop,
+            child: const Text('Close'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _showJson(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Sketch as JSON'),
+        content: SizedBox.expand(
+          child: SelectableText(
+            jsonEncode(notifier.currentSketch.toJson()),
+            autofocus: true,
+          ),
+        ),
+        actions: [
+          TextButton(
+            onPressed: Navigator.of(context).pop,
+            child: const Text('Close'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildStrokeToolbar(BuildContext context) {
+    return ValueListenableBuilder<ScribbleState>(
+      valueListenable: notifier,
+      builder: (context, state, _) => Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          for (final w in notifier.widths)
+            _buildStrokeButton(
+              context,
+              strokeWidth: w,
+              state: state,
+            ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildStrokeButton(
+    BuildContext context, {
+    required double strokeWidth,
+    required ScribbleState state,
+  }) {
+    final selected = state.selectedWidth == strokeWidth;
+    return Padding(
+      padding: const EdgeInsets.all(4),
+      child: Material(
+        elevation: selected ? 4 : 0,
+        shape: const CircleBorder(),
+        child: InkWell(
+          onTap: () => notifier.setStrokeWidth(strokeWidth),
+          customBorder: const CircleBorder(),
+          child: AnimatedContainer(
+            duration: kThemeAnimationDuration,
+            width: strokeWidth * 2,
+            height: strokeWidth * 2,
+            decoration: BoxDecoration(
+              color: state.map(
+                drawing: (s) => Color(s.selectedColor),
+                erasing: (_) => Colors.transparent,
+              ),
+              border: state.map(
+                drawing: (_) => null,
+                erasing: (_) => Border.all(width: 1),
+              ),
+              borderRadius: BorderRadius.circular(50.0),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildColorToolbar(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        _buildColorButton(context, color: Colors.black),
+        _buildColorButton(context, color: Colors.red),
+        _buildColorButton(context, color: Colors.green),
+        _buildColorButton(context, color: Colors.blue),
+        _buildColorButton(context, color: Colors.yellow),
+        _buildEraserButton(context),
+      ],
+    );
+  }
+
+  Widget _buildPointerModeSwitcher(BuildContext context) {
+    return ValueListenableBuilder(
+      valueListenable: notifier.select(
+        (value) => value.allowedPointersMode,
+      ),
+      builder: (context, value, child) {
+        return SegmentedButton<ScribblePointerMode>(
+          multiSelectionEnabled: false,
+          emptySelectionAllowed: false,
+          onSelectionChanged: (v) => notifier.setAllowedPointersMode(v.first),
+          segments: const [
+            ButtonSegment(
+              value: ScribblePointerMode.all,
+              icon: Icon(Icons.touch_app),
+              label: Text('All pointers'),
+            ),
+            ButtonSegment(
+              value: ScribblePointerMode.penOnly,
+              icon: Icon(Icons.draw),
+              label: Text('Pen only'),
+            ),
+          ],
+          selected: {value},
+        );
+      },
+    );
+  }
+
+  Widget _buildEraserButton(BuildContext context) {
+    return ValueListenableBuilder(
+      valueListenable: notifier.select((value) => value is Erasing),
+      builder: (context, value, child) => ColorButton(
+        color: Colors.transparent,
+        outlineColor: Colors.black,
+        isActive: value,
+        onPressed: () => notifier.setEraser(),
+        child: const Icon(Icons.cleaning_services),
+      ),
+    );
+  }
+
+  Widget _buildColorButton(
+    BuildContext context, {
+    required Color color,
+  }) {
+    return ValueListenableBuilder(
+      valueListenable: notifier.select(
+        (value) => value is Drawing && value.selectedColor == color.value,
+      ),
+      builder: (context, value, child) => Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 4),
+        child: ColorButton(
+          color: color,
+          isActive: value,
+          onPressed: () => notifier.setColor(color),
+        ),
+      ),
+    );
+  }
+}
+
+class ColorButton extends StatelessWidget {
+  const ColorButton({
+    required this.color,
+    required this.isActive,
+    required this.onPressed,
+    this.outlineColor,
+    this.child,
+    super.key,
+  });
+
+  final Color color;
+
+  final Color? outlineColor;
+
+  final bool isActive;
+
+  final VoidCallback onPressed;
+
+  final Icon? child;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: kThemeAnimationDuration,
+      decoration: ShapeDecoration(
+        shape: CircleBorder(
+          side: BorderSide(
+            color: switch (isActive) {
+              true => outlineColor ?? color,
+              false => Colors.transparent,
+            },
+            width: 2,
+          ),
+        ),
+      ),
+      child: IconButton(
+        style: FilledButton.styleFrom(
+          backgroundColor: color,
+          shape: const CircleBorder(),
+          side: isActive
+              ? const BorderSide(color: Colors.white, width: 2)
+              : const BorderSide(color: Colors.transparent),
+        ),
+        onPressed: onPressed,
+        icon: child ?? const SizedBox(),
+      ),
     );
   }
 }
